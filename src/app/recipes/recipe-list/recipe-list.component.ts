05296d51e172aace4b2234bbe532480a2ed92a96
dim <ilona.demchyshyna@gmail.com>
2023-04-17 17:32:15 +0300

    components and databinding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import { Recipe} from "../recipe.model";
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,12 +7,17 @@ import { Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output()
+  recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'this is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBsZGIZMkdLvAJd8--IdhBRze01osaLx0umw&usqp=CAU'),
-    new Recipe('A test recipe', 'this is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBsZGIZMkdLvAJd8--IdhBRze01osaLx0umw&usqp=CAU')
+    new Recipe('Another test recipe', 'this is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBsZGIZMkdLvAJd8--IdhBRze01osaLx0umw&usqp=CAU')
   ];
   constructor() {
   }
 ngOnInit() {
+}
+onRecipeSelected(recipe: Recipe) {
+this.recipeWasSelected.emit(recipe);
 }
 }
