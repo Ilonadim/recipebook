8f2ab69207b235724baf1e26559bb7cc84c05078
dim <ilona.demchyshyna@gmail.com>
2023-05-19 15:37:20 +0300

    services and dependency injection

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,23 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('Tasty Steak',
+    new Recipe(
+      'Tasty Steak',
       'A super easy way to prepare tasty steak',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBsZGIZMkdLvAJd8--IdhBRze01osaLx0umw&usqp=CAU'),
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBsZGIZMkdLvAJd8--IdhBRze01osaLx0umw&usqp=CAU',
     [
       new Ingredient('Meat', 1),
       new Ingredient('olive oil', 1)
     ]),
     new Recipe(
       'Burger',
-      'this is simply a test',
+      'burger recipe',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBsZGIZMkdLvAJd8--IdhBRze01osaLx0umw&usqp=CAU',
   [
     new Ingredient('Buns', 2),
@@ -22,7 +25,12 @@ export class RecipeService {
     ]
 )
   ];
+  constructor(private slService: ShoppingListService) {
+  }
   getRecipes() {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+this.slService.addIngredients(ingredients);
   }
 }
