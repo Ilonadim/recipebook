cccd45cd4bbbd5b3436e1dfe2d817f50e7c20258
dim <ilona.demchyshyna@gmail.com>
2023-06-26 16:48:40 +0300

    Routing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {relative} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,13 +10,24 @@ import {RecipeService} from "../recipe.service";
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-@Input()
   recipe: Recipe;
-constructor(private recipeService: RecipeService) {
+  id: number;
+constructor(private recipeService: RecipeService,
+            private route: ActivatedRoute,
+            private router: Router) {
 }
 ngOnInit() {
+  this.route.params.subscribe(
+    (params: Params) => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    }
+  );
 }
 onAddToShoppingList() {
 this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
 }
+  onEditRecipe() {
+this.router.navigate(['edit'], {relativeTo: this.route});
+  }
 }
