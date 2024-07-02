19e976d3a4fd90ef8f4132658eadfa23683c4cb1
dim <ilona.demchyshyna@gmail.com>
2024-07-02 11:37:16 +0300

    forms

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,14 @@
+import {Subject} from "rxjs";
 import {Recipe} from "./recipe.model";
 import {Injectable} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+  recipesChanges = new Subject()
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -35,5 +39,17 @@ export class RecipeService {
   }
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
 this.slService.addIngredients(ingredients);
+  }
+  addRecipe(recipe: Recipe) {
+this.recipes.push(recipe);
+this.recipesChanges.next(this.recipes.slice());
+  }
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanges.next(this.recipes.slice());
+  }
+  deleteRecipe(index: number) {
+    this.recipes.slice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
